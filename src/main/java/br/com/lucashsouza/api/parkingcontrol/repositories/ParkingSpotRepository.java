5f5807a4f967e84252a3daf11aebe07417561ee5
Lucas Souza <lhenriquesouza00@gmail.com>
2022-07-22 14:36:54 -0300

    first commit

---
+++ src/main/java/br/com/lucashsouza/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,15 @@
+package br.com.lucashsouza.api.parkingcontrol.repositories;
+
+import br.com.lucashsouza.api.parkingcontrol.models.ParkingSpot;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, UUID> {
+
+    boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
+}
---
+++ src/main/java/br/com/lucashsouza/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,52 @@
+package br.com.lucashsouza.api.parkingcontrol.services;
+
+import br.com.lucashsouza.api.parkingcontrol.models.ParkingSpot;
+import br.com.lucashsouza.api.parkingcontrol.repositories.ParkingSpotRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ParkingSpotService {
+
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    public Page<ParkingSpot> findAll(Pageable pageable) {
+        return parkingSpotRepository.findAll(pageable);
+    }
+
+    public Optional<ParkingSpot> findById(UUID id) {
+        return parkingSpotRepository.findById(id);
+    }
+
+    @Transactional
+    public ParkingSpot save(ParkingSpot parkingSpot) {
+        return parkingSpotRepository.save(parkingSpot);
+    }
+
+    public boolean existsByLicensePlateCar(String licensePlateCar) {
+        return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+    }
+
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+
+    public boolean existsByApartmentAndBlock(String apartment, String block) {
+        return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+    }
+
+    @Transactional
+    public void delete(ParkingSpot parkingSpot) {
+        parkingSpotRepository.delete(parkingSpot);
+    }
+}
